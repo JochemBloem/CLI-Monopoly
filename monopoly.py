1d2370fd123181be31e012a2cfd82f1a81a4b18f
= <=>
2019-04-20 19:35:10 +0200

    Added more features. Merged the .set and .properties commands.

--- monopoly.py
+++ monopoly.py
@@ -4,7 +4,7 @@ import time
 import boarddef_standard
 
 #globals
-GLOBAL_SPEED = 1.5
+GLOBAL_SPEED = .2
 SHORT_SLEEP = 1 * GLOBAL_SPEED
 LONG_SLEEP = 2 * GLOBAL_SPEED
 
@@ -181,6 +181,7 @@ class Player:
                         self.purchase(tile)
                 #the Property is owned by another player, and the current player has to pay rent
                 elif tile.state == "owned":
+                    print "You landed on " + tile.properties["name"]
                     print "This property is owned by " + self.game.players[tile.owner].name
                     if tile.properties["set"] == "utilities":
                         rent = total * (4 if game.getAmountInSet(tile.owner, "utilities") == 1 else 10)
@@ -244,21 +245,24 @@ class Player:
 
         return True
 
-    def build(self, tile):
-        return True
+    def build(self):
+        ownedSets = self.getOwnedSets()
+        for workingSet in ownedSets:
+            if game.getAmountInSet(self.index, workingset) == len(ownedSets[workingSet]):
+                print "You can build on the " + workingSet + " set."
+                return True
+        print "You need to have a complete set in order to start building houses. \nType .properties to see your current progress."
+        return False
 
-    def sets(self):
+    def getOwnedSets(self):
         ownedSets = {}
         for prop in self.properties:
             workingSet = prop.properties["set"]
             if not workingSet in ownedSets:
-                ownedSets[workingSet] = 1
+                ownedSets[workingSet] = [prop]
             else:
-                ownedSets[workingSet] += 1
-        
-        for key in ownedSets:
-            print key + ": " + str(game.getAmountInSet(self.index, key)) + "/" + str(game.getTotalInSet(key))
-        return False
+                ownedSets[workingSet].append(prop)
+        return ownedSets
 
     def bail(self):
         return True
@@ -284,20 +288,32 @@ class Player:
         clearSpace(1)
         return False
 
+    def sets(self):
+        ownedSets = self.getOwnedSets()
+        
+        for key in ownedSets:
+            print key + ": " + str(game.getAmountInSet(self.index, key)) + "/" + str(game.getTotalInSet(key))
+        return False
+
     def showProperties(self):
         clearSpace(1)
         print "You own the following properties:"
-        for prop in self.properties:
-            name = prop.properties["name"] + " (" + prop.properties["set"] + " set), "
-            while len(name) < 30:
-                name += " "
-            houses = "with " + (str(prop.houses) + " house" + ("s" if not prop.houses == 1 else "") if not prop.houses == 5 else "1 hotel") + ", "
-            while len(houses) < 15:
-                houses += " "
-            rent = "which " + ("costs" if not prop.state == "mortgaged" else "would cost") + " $" + str(prop.properties["rent"][prop.houses])
-
-            mortgage = ("" if not prop.state == "mortgaged" else ", but it is mortgaged")
-            print name + houses + rent + mortgage
+        
+        ownedSets = self.getOwnedSets()
+        
+        for key in ownedSets:
+            print key + ": " + str(len(ownedSets[key])) + "/" + str(game.getTotalInSet(key))
+            for prop in ownedSets[key]:
+                name = "    " + prop.properties["name"]
+                while len(name) < 35:
+                    name += " "
+                houses = "with " + (str(prop.houses) + " houses " if not prop.houses == 5 else "1 hotel ") if prop.properties["set"] not in ["utilities", "railroads"] else ""
+                while len(houses) < 14:
+                    houses += " "
+                multiplier = (2 if game.getAmountInSet(self.index, prop.properties["set"]) == game.getTotalInSet(prop.properties["set"]) else 1)
+                rent = "($" + str(prop.properties["rent"][(prop.houses if prop.properties["set"] not in ["utilities", "railroads"] else game.getAmountInSet(self.index, prop.properties["set"]) )] * multiplier) + ")"
+                mortgaged = ("\nMORTGAGED" if prop.state == "mortgaged" else "")
+                print name + houses + rent + mortgaged
         clearSpace(1)
         return False
 
